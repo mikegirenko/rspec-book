1701a21bca4d84195b5a8d3a8197a101bcea8e10
mike.girenko <mike.girenko@cybergrx.com>
2020-12-08 17:44:52 -0700

    PEP8

--- codebreaker/features/steps/codebreaker_steps.py
+++ codebreaker/features/steps/codebreaker_steps.py
@@ -8,11 +8,13 @@ from codebreaker import Game, Output
 def step_impl(context):
     pass
 
+
 @when('I start a new game')
 def step_impl(context):
     context.output = Output()
     context.game = Game(context.output)
     context.game.start()
+
 
 @then('I should see "{message}"')
 def step_impl(context, message):
